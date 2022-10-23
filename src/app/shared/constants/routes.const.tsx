3b3138fb82a91d95f94cb14dc08452c0b31e9f31
ristespasov <ristespasov@hotmail.com>
2022-10-23 14:28:53 +0200

    routes constants defined

---
+++ src/app/shared/constants/routes.const.tsx
@@ -0,0 +1,14 @@
+export const routes = [
+  {
+    path: "movies",
+    name: "Movies",
+  },
+  {
+    path: "auth/login",
+    name: "Login",
+  },
+  {
+    path: "auth/register",
+    name: "Register",
+  },
+];
