246499e0851c368205a8396a74ae236d60c8829b
ristespasov <ristespasov@hotmail.com>
2022-10-23 14:54:13 +0200

    header links handled

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { IRoute } from 'src/app/shared/interfaces/route.interface';
+import { routes } from '../../shared/constants/routes.const';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  routes: Array<IRoute>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.routes = routes;
   }
-
 }
