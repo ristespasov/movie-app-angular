22f816f5d77bd88e94809fee53185e8f84552a5c
ristespasov <ristespasov@hotmail.com>
2022-10-23 18:56:44 +0200

    show movies implemented

--- src/app/feature/movies/movies.service.ts
+++ src/app/feature/movies/movies.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { api } from 'src/app/shared/constants/api.const';
+import { endpoints } from 'src/app/shared/constants/endpoints.const';
+import { IMoviesDataResponse } from 'src/app/shared/interfaces/movies-data.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getPopularMovies(): Observable<IMoviesDataResponse> {
+    return this.http.get<IMoviesDataResponse>(
+      endpoints.baseUrl + endpoints.popularMoviesUrl + '?api_key=' + api.apiKey
+    );
+  }
 }
--- src/app/feature/movies/movies/movies.component.ts
+++ src/app/feature/movies/movies/movies.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { endpoints } from 'src/app/shared/constants/endpoints.const';
+import { IMovieResponse } from 'src/app/shared/interfaces/movie.interface';
+import { MoviesService } from '../movies.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.scss']
+  styleUrls: ['./movies.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MoviesComponent implements OnInit {
+  responseMessage: string;
+  posterBaseUrl: string = endpoints.posterbaseUrl;
+  popularMovies: Array<IMovieResponse>;
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
+    this.moviesService.getPopularMovies().subscribe({
+      next: (response) => {
+        this.popularMovies = response.results;
+      },
+      error: (err) => {
+        this.responseMessage = err.error.status_message;
+      },
+    });
   }
-
 }
