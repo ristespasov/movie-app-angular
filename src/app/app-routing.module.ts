1f9cfbf4ea7e6ec62d45257ab07056c3382ed439
ristespasov <ristespasov@hotmail.com>
2022-10-23 14:30:03 +0200

    base routes defined

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './feature/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./feature/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'movies',
+    loadChildren: () =>
+      import('./feature/movies/movies.module').then((m) => m.MoviesModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'movies',
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/feature/movies/movies-routing.module.ts
+++ src/app/feature/movies/movies-routing.module.ts
@@ -1,5 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+import { MoviesComponent } from './movies/movies.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MoviesComponent,
+  },
+];
 
 @NgModule({
   declarations: [],
