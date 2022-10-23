a13ad62402efac4e04a5c7ebb554b21bf00c4dd6
ristespasov <ristespasov@hotmail.com>
2022-10-23 18:55:07 +0200

    constants defined

---
+++ src/app/shared/constants/api.const.ts
@@ -0,0 +1,3 @@
+export const api = {
+  apiKey: 'af27f914e6a94e470e0d67b9061befb7',
+};
---
+++ src/app/shared/constants/endpoints.const.ts
@@ -0,0 +1,5 @@
+export const endpoints = {
+  posterbaseUrl: 'https://image.tmdb.org/t/p/original/',
+  baseUrl: 'https://api.themoviedb.org/3/movie/',
+  popularMoviesUrl: 'popular',
+};
