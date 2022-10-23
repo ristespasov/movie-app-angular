86f618dec6a0cb221173997b79d2ef2f95b50b86
ristespasov <ristespasov@hotmail.com>
2022-10-23 21:37:49 +0200

    paginator and query params implemented

--- src/app/feature/movies/movies.module.ts
+++ src/app/feature/movies/movies.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CoreModule } from 'src/app/core/core.module';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [MoviesComponent, MovieDetailsComponent],
-  imports: [CommonModule, MoviesRoutingModule],
+  imports: [CommonModule, CoreModule, MoviesRoutingModule],
 })
 export class MoviesModule {}
--- src/app/feature/movies/movies/movies.component.ts
+++ src/app/feature/movies/movies/movies.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 import { endpoints } from 'src/app/shared/constants/endpoints.const';
 import { IMovieResponse } from 'src/app/shared/interfaces/movie.interface';
+import { PaginationConfigModel } from 'src/app/shared/models/pagination.model';
 import { MoviesService } from '../movies.service';
 
 @Component({
@@ -10,20 +18,69 @@ import { MoviesService } from '../movies.service';
   encapsulation: ViewEncapsulation.None,
 })
 export class MoviesComponent implements OnInit {
-  responseMessage: string;
+  isLoading = false;
   posterBaseUrl: string = endpoints.posterbaseUrl;
+  responseMessage: string;
   popularMovies: Array<IMovieResponse>;
+  paginationConfig: PaginationConfigModel;
+  currentPageNumber: number;
+  queryPageNumber: number;
 
-  constructor(private moviesService: MoviesService) {}
+  constructor(
+    private moviesService: MoviesService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.moviesService.getPopularMovies().subscribe({
-      next: (response) => {
-        this.popularMovies = response.results;
-      },
-      error: (err) => {
-        this.responseMessage = err.error.status_message;
-      },
-    });
+    this.isLoading = true;
+    this.route.queryParams
+      .pipe(
+        switchMap((params: Params) =>
+          this.moviesService.getPopularMovies(
+            (this.queryPageNumber = params?.['page'])
+          )
+        )
+      )
+      .subscribe({
+        next: (response) => {
+          this.isLoading = false;
+          this.popularMovies = response?.results;
+          this.currentPageNumber = 1;
+          this.paginationConfig = {
+            itemsPerPage: this.popularMovies.length,
+            currentPage: this.queryPageNumber,
+            totalItems: response.total_pages,
+          };
+          this.cdr.markForCheck();
+          console.log(response);
+        },
+        error: (err) => {
+          this.isLoading = false;
+          this.responseMessage = err?.error?.status_message;
+        },
+        complete: () => {
+          this.isLoading = false;
+        },
+      });
+  }
+
+  setPageQueryParam(page: number) {
+    const queryParams: Params = {
+      page: page,
+    };
+    if (page) {
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: queryParams,
+        queryParamsHandling: 'merge',
+      });
+    }
+  }
+
+  onPageChange(event: any) {
+    this.currentPageNumber = event;
+    this.setPageQueryParam(this.currentPageNumber);
   }
 }
