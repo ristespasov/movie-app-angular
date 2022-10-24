0621398e08a65b36a43601d74cad6603ecf78909
ristespasov <ristespasov@hotmail.com>
2022-10-24 16:56:42 +0200

    module update

--- src/app/feature/movies/movies-routing.module.ts
+++ src/app/feature/movies/movies-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesComponent } from './movies/movies.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MoviesComponent,
+  },
+  {
+    path: ':id',
+    component: MovieDetailsComponent,
   },
 ];
 
--- src/app/feature/movies/movies.module.ts
+++ src/app/feature/movies/movies.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CoreModule } from 'src/app/core/core.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [MoviesComponent, MovieDetailsComponent],
-  imports: [CommonModule, CoreModule, MoviesRoutingModule],
+  imports: [CommonModule, CoreModule, SharedModule, MoviesRoutingModule],
 })
 export class MoviesModule {}
