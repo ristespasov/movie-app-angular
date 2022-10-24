dbbbf7da3233d622ae90455b71c5b337671c8c03
ristespasov <ristespasov@hotmail.com>
2022-10-24 12:10:36 +0200

    movie details method defined

--- src/app/feature/movies/movies.service.ts
+++ src/app/feature/movies/movies.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { api } from 'src/app/shared/constants/api.const';
 import { endpoints } from 'src/app/shared/constants/endpoints.const';
+import { IMovieDetails } from 'src/app/shared/interfaces/movie-details.interface';
 import { IMoviesDataResponse } from 'src/app/shared/interfaces/movies-data.interface';
 
 @Injectable({
@@ -16,6 +17,12 @@ export class MoviesService {
     return this.http.get<IMoviesDataResponse>(
       endpoints.baseUrl + endpoints.popularMoviesUrl + '?api_key=' + api.apiKey,
       { params: params }
+    );
+  }
+
+  getMovieDetails(id: string): Observable<IMovieDetails> {
+    return this.http.get<IMovieDetails>(
+      endpoints.baseUrl + id + '?api_key=' + api.apiKey
     );
   }
 }
