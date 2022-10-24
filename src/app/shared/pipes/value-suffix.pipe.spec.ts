da90a118d670ba6661411fad7d10ef33b3bd3d2f
ristespasov <ristespasov@hotmail.com>
2022-10-24 23:35:29 +0200

    value suffix pipe defined

---
+++ src/app/shared/pipes/value-suffix.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ValueSuffixPipe } from './value-suffix.pipe';
+
+describe('ValueSuffixPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ValueSuffixPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
